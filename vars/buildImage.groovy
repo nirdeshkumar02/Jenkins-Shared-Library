3624838e0ffbdfdabc36e73e812e9b0feebe7940
nirdeshkumar02 <saini.nirdesh.kumar@gmail.com>
2022-12-16 20:40:23 +0530

    parameterized image name

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,16 +1,24 @@
 #!/usr/bin/env groovy
 
-def call() {
+//  Using Parameterized Value, Taking Image name from Jenkinsfile
+
+def call(String imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nirdeshkumar02/jenkins-java-app:jma-3 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nirdeshkumar02/jenkins-java-app:jma-3'
+        sh "docker push $imageName"
     }
 }
 
-// import com.example.Docker
 
-// def call(String imageName) {
-//     return new Docker(this).buildDockerImage(imageName)
+// Used HardCoded Value
+
+// def call() {
+//     echo "building the docker image..."
+//     withCredentials([usernamePassword(credentialsId: 'docker-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//         sh 'docker build -t nirdeshkumar02/jenkins-java-app:jma-3 .'
+//         sh "echo $PASS | docker login -u $USER --password-stdin"
+//         sh 'docker push nirdeshkumar02/jenkins-java-app:jma-3'
+//     }
 // }
